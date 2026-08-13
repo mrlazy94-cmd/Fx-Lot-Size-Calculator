217f629b2f686bfab7c1db8a2e26bb0b9434fb0a
mrlazy94-cmd <mrlazy94@hotmail.co.uk>
2026-08-13 21:49:46 +0100

    Add files via upload

---
+++ fxcm-specs.js
@@ -0,0 +1,7 @@
+const FXCM_SPECS = {
+  "SPX500": { valuePerUnit: 1.0, volumeStep: 1.00, unitLabel: "point" },
+  "XAU/USD": { valuePerUnit: 1.0, volumeStep: 1.00, unitLabel: "dollar ($)" },
+  "XAG/USD": { valuePerUnit: 1.0, volumeStep: 1.00, unitLabel: "dollar ($)" },
+  "XCU/USD": { valuePerUnit: 100.0, volumeStep: 1.00, unitLabel: "dollar ($)" },
+  "USOilSpot": { valuePerUnit: 10.0, volumeStep: 1.00, unitLabel: "dollar ($)" }
+};
