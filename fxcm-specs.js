0d16c90728b8a09ee0d5957660e76f5fa0706e23
mrlazy94-cmd <mrlazy94@hotmail.co.uk>
2026-08-15 07:11:42 +0100

    Delete fxcm-specs.js

--- fxcm-specs.js
+++
@@ -1,7 +0,0 @@
-const FXCM_SPECS = {
-  "SPX500": { valuePerUnit: 1.0, volumeStep: 1.00, unitLabel: "point" },
-  "XAU/USD": { valuePerUnit: 1.0, volumeStep: 1.00, unitLabel: "dollar ($)" },
-  "XAG/USD": { valuePerUnit: 1.0, volumeStep: 1.00, unitLabel: "dollar ($)" },
-  "XCU/USD": { valuePerUnit: 100.0, volumeStep: 1.00, unitLabel: "dollar ($)" },
-  "USOilSpot": { valuePerUnit: 10.0, volumeStep: 1.00, unitLabel: "dollar ($)" }
-};
